34108c795d973e67c45cfbc0a68bb2f532f54d8a
GoLift Dev Team <dev@golift.com>
2026-02-28 18:27:40 +0000

    Update: syntax corrigida

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ if (!process.env.JWT_SECRET || process.env.JWT_SECRET === 'golift_super_secret')
   process.exit(1);
 }
 
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const app = express();
+const http = require('http');
+const os = require('os');
+
 // Modular route imports (after app is initialized)
 const recordesRoutes = require('./routes/recordes/recordes.routes');
 const sessoesRoutes = require('./routes/sessoes/sessoes.routes');
